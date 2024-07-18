80bc4c2dba7772936ad795665ff4dcf4af47643d
0x7C2f <0x7C2f@grimdevelopment.com>
2024-07-18 12:49:40 -0400

    update 7 files

--- build/esbuild.js
+++ build/esbuild.js
@@ -6,12 +6,14 @@ require('dotenv').config();
 const main_path = path.join(process.cwd(), 'dist', 'main.js');
 const manifest_path = path.join(process.cwd(), 'manifest.json');
 const styles_path = path.join(process.cwd(), 'src', 'styles.css');
+
 // Update manifest.json version
 const package_json = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'package.json')));
 const manifest_json = JSON.parse(fs.readFileSync(manifest_path));
 manifest_json.version = package_json.version;
 fs.writeFileSync(manifest_path, JSON.stringify(manifest_json, null, 2));
-// copy manifest and styles to dist
+
+// Copy manifest and styles to dist
 fs.copyFileSync(manifest_path, path.join(process.cwd(), 'dist', 'manifest.json'));
 fs.copyFileSync(styles_path, path.join(process.cwd(), 'dist', 'styles.css'));
 
@@ -20,11 +22,11 @@ const destination_vaults = [
   'obsidian-1',
 ];
 
-// get first argument as entry point
+// Get first argument as entry point
 const entry_point = process.argv[2] || 'src/index.js';
 
-// Build the project
-esbuild.build({
+// Build options
+const buildOptions = {
   entryPoints: [entry_point],
   outfile: 'dist/main.js',
   format: 'cjs',
@@ -32,7 +34,7 @@ esbuild.build({
   write: true,
   sourcemap: 'inline',
   target: "es2018",
-	logLevel: "info",
+  logLevel: "info",
   treeShaking: true,
   platform: 'node',
   preserveSymlinks: true,
@@ -42,13 +44,20 @@ esbuild.build({
     'crypto',
     '@xenova/transformers',
   ],
-  define: {
+};
+
+// Conditionally add the define property
+if (process.env.DEFAULT_OPEN_ROUTER_API_KEY) {
+  buildOptions.define = {
     'process.env.DEFAULT_OPEN_ROUTER_API_KEY': JSON.stringify(process.env.DEFAULT_OPEN_ROUTER_API_KEY),
-  },
-}).then(() => {
+  };
+}
+
+// Build the project
+esbuild.build(buildOptions).then(() => {
   // Copy the dist folder to ./DESTINATION_VAULT/.obsidian/plugins/smart-connections/
   const release_file_paths = [manifest_path, styles_path, main_path];
-  for(let vault of destination_vaults) {
+  for (let vault of destination_vaults) {
     const destDir = path.join(process.cwd(), '..', vault, '.obsidian', 'plugins', 'smart-connections');
     fs.mkdirSync(destDir, { recursive: true });
     release_file_paths.forEach(file_path => {
